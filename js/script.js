b15d2cbb8a502345532db4426d5c935954b7b8e0
Seth Buchinger <seth.buchinger@icloud.com>
2021-07-28 16:38:44 -0400

    Added check boxes to plot SDS and STEPS model data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+var HEIGHT,WIDTH,MARGINS
+
 var dataset;
 var projDataset;
+var subActGrouped;
+var subProjGrouped;
+var subsetActData;
+var projSDSDataset;
+var subProjSDSGrouped;
 // slider
 var sMargin = {top:0, right:50, bottom:0, left:50},
     sWidth = 960 - sMargin.left - sMargin.right,
@@ -24,6 +31,7 @@ var x = d3.scaleTime()
 var slider = svgSlider.append("g")
     .attr("class","slider")
     .attr("transform", "translate(" + sMargin.left + "," + sHeight /2 + ")");
+
 
 slider.append("line")
     .attr("class","track")
@@ -51,7 +59,8 @@ slider.insert("g", ".track-overlay")
 
 var handle = slider.insert("circle", ".track-overlay")
     .attr("class", "handle")
-    .attr("r", 9);
+    .attr("r", 9)
+    .attr("cx",860)
 
 var label = slider.append("text")
     .attr("class", "label")
@@ -79,28 +88,25 @@ var idScene2 = document.getElementById('scene_2');
 var idScene3 = document.getElementById('scene_3');
 var idSlider = document.getElementById('slider');
 
+var idCheckbox1 = document.getElementById('checkbox1');
+var idCheckbox2 = document.getElementById('checkbox2');
 
-
-function drawLegend(dataGroup){
-
+function drawLegend(dataGroup,colors){
     //initialize legend
     var legendItemSize = 8;
     var legendSpacing = 4;
-    var xOffset = 150;
-    var yOffset = 0;
+    var xOffset = 0;
+    var yOffset = 5;
     var legend = d3.select('#legend').append('svg')
         .selectAll('.legendItem')
         .data(dataGroup);
 
-    color = d3.scaleLinear()
-        .domain([0, 2, 4, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 34])
-        .range(["red", "palegreen", "yellow", "blue", "aqua", "coral", "darkviolet", "seagreen", "skyblue", "peachpuff", "royalblue", "magenta", "navy", "olive", "orchid"])
 
     //create legend items
     legend.enter()
         .append('text')
         .attr('x', xOffset + legendItemSize +5)
-        .attr('y', (d,i) => yOffset + (legendItemSize + legendSpacing) * i +12)
+        .attr('y', (d,i) => yOffset + (legendItemSize + legendSpacing) * i +8)
         .text(d=>d.key);
 
 
@@ -110,7 +116,7 @@ function drawLegend(dataGroup){
             //.attr('id', 'legendItem_', + d.key);
             .attr('height', legendItemSize)
             .attr('fill', function (d, i) {
-                return color(i)
+                return colors(i)
             })
             .attr('transform',
                 (d, i) => {
@@ -136,6 +142,7 @@ function switchToScene1() {
     //load dive elements
         d3.selectAll("path").remove();
         d3.select('#visualisation').selectAll("circle").remove()
+        d3.select('#legend').selectAll("svg").remove()
         idScene1.style.display = 'block';
         idScene2.style.display = 'none';
         idSlider.style.display = 'block';
@@ -150,12 +157,19 @@ function switchToScene1() {
             d.year = new Date(+d.year, 0, 1)
         });
 
+        colors = d3.scaleLinear()
+            .domain([0, 2, 4, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 34])
+            .range(["red", "palegreen", "yellow", "blue", "aqua", "coral", "darkviolet", "seagreen", "skyblue", "peachpuff", "royalblue", "magenta", "navy", "olive", "orchid"])
 
         //console.log(JSON.stringify(dataGroup))
         dataset = data;
+        var easeInto = d3.easeLinear;
+       var newDuration = 3000;
        var groupedData = groupByCountry(dataset);
-        drawLegend(groupedData);
-        drawAxes(dataset)
+        drawLegend(groupedData,colors);
+        drawAxes(dataset,70);
+        drawPlot(dataset,newDuration,colors);
+        axesLabels();
 
 
 
@@ -173,7 +187,7 @@ function switchToScene1() {
             .entries(data);
 
     }
-    function drawAxes(data){
+    function drawAxes(data,cap){
         // set vars: dimensions and margins of the graph
         var yTicks = 6;
         var xTicks = 11;
@@ -206,7 +220,7 @@ function switchToScene1() {
             })])
             .range([MARGINS.left * 2, WIDTH - MARGINS.right]),
             yScale = d3.scaleLinear()
-                .domain([0, 50])
+                .domain([0, cap])
                 .range([HEIGHT - MARGINS.top, MARGINS.bottom]),
             xAxis = d3.axisBottom()
                 .scale(xScale)
@@ -254,7 +268,7 @@ function switchToScene1() {
 
     }
 
-    function drawPlot(data) {
+    function drawPlot(data,duration,colors) {
         //console.log(data[0]);
 
         //nest the data
@@ -267,7 +281,7 @@ function switchToScene1() {
             .y(function (d) {
                 return yScale(d.value);
             })
-            .curve(d3.curveBasis);
+            //.curve(d3.curveBasis);
 
         // create the line graph
 
@@ -279,11 +293,14 @@ function switchToScene1() {
                 .attr('d', pathData)
                 .attr('class', 'line')
                 .attr('id', 'line_' + d.key)
+                //.select('path').transition().duration(20)
                 .attr('stroke-width', 2)
                 .attr('stroke', function (d, j) {
-                    return color(i)
+                    return colors(i)
                 })
                 .attr("fill", "none")
+
+
 
 
                 //create hover tooltip
@@ -300,14 +317,28 @@ function switchToScene1() {
                 .on('mouseout', function () {
                     tooltip.html('')
                 })
+                .attr("stroke-dasharray", function (){
+                    return pathLength = this.getTotalLength();
+                })
+                .attr("stroke-dashoffset",pathLength)
+                .transition()
+                .ease(d3.easeLinear)
+                .duration(duration)
+                .attr("stroke-dashoffset", 0)
+
+
 
 
         });
 
 
+
+
     }
 
 function update(h) {
+    var upDuration = 0;
+    var easeBack = d3.easePolyOut;
     handle.attr("cx", x(h));
     label
         .attr("x", x(h))
@@ -318,15 +349,35 @@ function update(h) {
         return d.year < h;
     })
     d3.selectAll("path").remove();
-    drawPlot(newData);
+    drawPlot(newData,upDuration);
 }
 
+
+function axesLabels(){
+
+    vis.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "middle")
+        .attr("y", 60 - MARGINS.left)
+        .attr("x",0 - (HEIGHT / 2))
+        .attr("dy", "1em")
+        .attr("transform", "rotate(-90)")
+        .text("EV Adoption Rate (% market share)");
+
+    vis.append("text")
+        .attr("transform",
+            "translate(" + (WIDTH/2) + " ," +
+            (HEIGHT + MARGINS.top + 10) + ")")
+        .style("text-anchor", "middle")
+        .text("Date");
+}
 
 function switchToScene2(){
 
     d3.selectAll("path").remove();
     d3.select('#visualisation').selectAll("circle").remove()
     d3.select('#visualisation').selectAll("g").remove()
+    d3.select('#legend').selectAll("svg").remove()
     idScene1.style.display = 'none';
     idScene2.style.display = 'block';
     idSlider.style.display = 'none';
@@ -337,9 +388,22 @@ function switchToScene2(){
     var subsetData = dataset.filter(function (d) {
         return d.region === 'Norway'|| d.region === 'USA' || d.region ==='World';
     })
+    var groupedData = groupByCountry(subsetData);
 
-    drawAxes(subsetData);
-    drawPlot(subsetData);
+   /* color = d3.scaleLinear()
+        .domain([0, 2, 4, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 34])
+        .range(["red", "palegreen", "yellow", "blue", "aqua", "coral", "darkviolet", "seagreen", "skyblue", "peachpuff", "royalblue", "magenta", "navy", "olive", "orchid"])
+*/
+    //Norway 255,218,185
+//US 128,128,0
+//World 173,120,107
+   var colors = d3.scaleLinear()
+        .domain([0,1,2])
+        .range(["rgb(255,218,185)","rgb(128,128,0)","rgb(173,120,107)"])
+    drawAxes(subsetData,80);
+    drawPlot(subsetData,2000,colors);
+    drawLegend(groupedData,colors);
+    axesLabels();
     addAnnotations();
 
 
@@ -433,17 +497,21 @@ function addAnnotations() {
 }
 
 
+
 function switchToScene3() {
 
     d3.selectAll("path").remove();
     d3.select('#visualisation').selectAll("circle").remove()
     d3.select('#visualisation').selectAll("g").remove()
+    d3.select('#legend').selectAll("svg").remove()
     idScene1.style.display = 'none';
     idScene2.style.display = 'none';
     idSlider.style.display = 'none';
     idScene3.style.display = 'block';
+    idCheckbox1.style.display = 'block';
+    idCheckbox2.style.display = 'block';
 
-    d3.csv('js/data/IEA-EV-dataEV sales shareCarsProjection-Combined.csv', function (data) {
+    d3.csv('js/data/IEA-EV-dataEV sales shareCarsProjection-CombinedSTEPS.csv', function (data) {
         parseDate = d3.timeParse("%Y")
         data.forEach(function (d) {
             d.value = +d.value;
@@ -455,18 +523,272 @@ function switchToScene3() {
         //filter only actual data points
         var actDateFilt = new Date(2020, 1, 1)
         console.log(actDateFilt)
-        var subsetActData = projDataset.filter(function (d) {
+        subsetActData = projDataset.filter(function (d) {
             return d.year < actDateFilt
         })
         // var subProjData = projDataset.filter(function (d) {
         //     return d.year >= actDateFilt
         // })
 
-        var subActGrouped = groupByCountry(subsetActData);
-        var subProjGrouped = groupByCountry(projDataset);
+        subActGrouped = groupByCountry(subsetActData);
+        subProjGrouped = groupByCountry(projDataset);
         //plot US vs. Norway
 
-        drawAxes(projDataset);
+
+        drawAxes(projDataset, 80);
+        plotSubActGrouped();
+
+
+        /*     var checkbox1 = d3.select('body')
+                 .append('div')
+                 .attr('id','checkbox1')
+                 .style('float','left')
+                 .style('margin-left','900px')
+                 .style('position','fixed')
+                 .append('label')
+                 .attr('for','model1')
+                 .text("STEPS")
+                 .append('input')
+                 .attr('type','checkbox')
+                 .attr('id', 'model1')
+                 .attr('name','model1')
+                 .attr('value',"Model1")
+                 .attr('onchange',"plotSTEPS(this)")
+
+
+
+             var checkbox2 = d3.select('body')
+
+                 .append('div')
+                 .attr('id','checkbox2')
+                 .style('float','left')
+                 .style('margin-left','1000px')
+                 .style('position','fixed')
+                 .append('label')
+                 .attr('for','model3')
+                 .text("SDS")
+                 .append('input')
+                 .attr('type','checkbox')
+                 .attr('id', 'model1')
+                 .attr('name','model1')
+                 .attr('value',"Model1")*/
+
+
+
+
+
+
+    })
+
+
+
+
+}
+
+//create plot of projected data
+function plotSTEPS(checkboxElem) {
+    if (checkboxElem.checked) {
+        plotSubProjGrouped()
+    } else {
+        d3.select('#visualisation').selectAll("circle").remove()
+        d3.selectAll("path").remove();
+        plotSubActGrouped();
+        var cbSDS =  document.getElementById('SDS');
+
+        if (cbSDS.checked){
+            plotSubProjSDSGrouped()
+        }
+    }
+}
+function plotSubProjGrouped() {
+
+    var lineGen = d3.line()
+        .x(function (d) {
+            return xScale(formatYear(d.year));
+        })
+        .y(function (d) {
+            return yScale(d.value);
+        })
+    subProjGrouped.forEach(function (d, i) {
+        var pathData2 = lineGen(d.values);
+        vis.append('path')
+            .attr('d', pathData2)
+            .attr('class', 'line')
+            .attr('id', 'line_' + d.key)
+            .attr('stroke-width', 2)
+            .attr('stroke-dasharray', 4)
+            .attr('stroke', function (d, j) {
+                return color(i)
+            })
+            .attr("fill", "none")
+
+            //create hover tooltip
+            .on('mouseover', function () {
+                // console.log(d)
+                tooltip.transition().duration(200)
+                    .style('opacity', .9)
+                tooltip.html(
+                    '<div style="font-family:sans-serif; font-size: 1rem; font-weight: bold">' + d.key + '</div>'
+                )
+                    .style('left', (d3.event.pageX - 35) + 'px')
+                    .style('top', (d3.event.pageY - 30) + 'px')
+            })
+            .on('mouseout', function () {
+                tooltip.html('')
+            })
+        vis.selectAll('myCircles')
+            .data(projDataset)
+            .enter()
+            .append('circle')
+            .attr("fill", "lightgrey")
+            .attr("stroke", "none")
+            .attr("cx", function (d) {
+                return xScale(formatYear(d.year))
+            })
+            .attr("cy", function (d) {
+                return yScale(d.value)
+            })
+            .attr("r", 3)
+            .on('mouseover', function (d) {
+                // console.log(d)
+                tooltip.transition().duration(200)
+                    .style('opacity', .9)
+                tooltip.html(
+                    '<div style="font-family:sans-serif; font-size: 1rem; font-weight: bold">' + d.value + '</div>'
+                )
+                    .style('left', (d3.event.pageX - 35) + 'px')
+                    .style('top', (d3.event.pageY - 30) + 'px')
+            })
+            .on('mouseout', function () {
+                tooltip.html('')
+
+            })
+
+    })
+
+
+}
+function plotSubActGrouped() {
+
+    var lineGen = d3.line()
+        .x(function (d) {
+            return xScale(formatYear(d.year));
+        })
+        .y(function (d) {
+            return yScale(d.value);
+        })
+
+//.curve(d3.curveBasis);
+
+// create the line graph
+
+    subActGrouped.forEach(function (d, i) {
+
+
+        var pathData1 = lineGen(d.values);
+        vis.append('path')
+            .attr('d', pathData1)
+            .attr('class', 'line')
+            .attr('id', 'line_' + d.key)
+            .attr('stroke-width', 2)
+            .attr('stroke', function (d, j) {
+                return color(i)
+            })
+            .attr("fill", "none")
+
+            //create hover tooltip
+            .on('mouseover', function () {
+                // console.log(d)
+                tooltip.transition().duration(200)
+                    .style('opacity', .9)
+                tooltip.html(
+                    '<div style="font-family:sans-serif; font-size: 1rem; font-weight: bold">' + d.key + '</div>'
+                )
+                    .style('left', (d3.event.pageX - 35) + 'px')
+                    .style('top', (d3.event.pageY - 30) + 'px')
+            })
+            .on('mouseout', function () {
+                tooltip.html('')
+            })
+
+
+        vis.selectAll('myCircles')
+            .data(subsetActData)
+            .enter()
+            .append('circle')
+            .attr("fill", "lightgrey")
+            .attr("stroke", "none")
+            .attr("cx", function (d) {
+                return xScale(formatYear(d.year))
+            })
+            .attr("cy", function (d) {
+                return yScale(d.value)
+            })
+            .attr("r", 3)
+            .on('mouseover', function (d) {
+                // console.log(d)
+                tooltip.transition().duration(200)
+                    .style('opacity', .9)
+                tooltip.html(
+                    '<div style="font-family:sans-serif; font-size: 1rem; font-weight: bold">' + d.value + '</div>'
+                )
+                    .style('left', (d3.event.pageX - 35) + 'px')
+                    .style('top', (d3.event.pageY - 30) + 'px')
+            })
+            .on('mouseout', function () {
+                tooltip.html('')
+
+            })
+
+        var projThresh = new Date(2020, 0, 1);
+
+        vis.append('line')
+            .attr('x1', 490)
+            .attr('y1', 439)
+            .attr('x2', 490)
+            .attr('y2', 150)
+            .style("stroke-width", 2)
+            .style("stroke", "lightgrey")
+            .style("stroke-dasharray", 3)
+            .style("fill", "none");
+
+
+    })
+}
+
+function plotSDS(checkboxElem) {
+    if (checkboxElem.checked) {
+        plotSubProjSDSGrouped()
+    }
+
+    else {
+        d3.select('#visualisation').selectAll("circle").remove()
+        d3.selectAll("path").remove();
+        plotSubActGrouped();
+
+        var cbSTEPS =  document.getElementById('STEPS');
+
+        if (cbSTEPS.checked){
+            plotSubProjGrouped()
+        }
+
+    }
+}
+function plotSubProjSDSGrouped()
+{
+    d3.csv('js/data/IEA-EV-dataEV sales shareCarsProjection-CombinedSDS.csv', function (data) {
+        parseDate = d3.timeParse("%Y")
+        data.forEach(function (d) {
+            d.value = +d.value;
+            d.value = +d.value;
+            d.year = new Date(+d.year, 0, 1)
+        });
+        projSDSDataset = data;
+
+        subProjSDSGrouped = groupByCountry(projSDSDataset);
+        //plot US vs. Norway
+
+
         var lineGen = d3.line()
             .x(function (d) {
                 return xScale(formatYear(d.year));
@@ -474,22 +796,14 @@ function switchToScene3() {
             .y(function (d) {
                 return yScale(d.value);
             })
-
-
-        //.curve(d3.curveBasis);
-
-        // create the line graph
-
-        console.log(subActGrouped[0]);
-        subActGrouped.forEach(function (d, i) {
-
-
-            var pathData1 = lineGen(d.values);
+        subProjSDSGrouped.forEach(function (d, i) {
+            var pathData2 = lineGen(d.values);
             vis.append('path')
-                .attr('d', pathData1)
+                .attr('d', pathData2)
                 .attr('class', 'line')
                 .attr('id', 'line_' + d.key)
                 .attr('stroke-width', 2)
+                .attr('stroke-dasharray', 4)
                 .attr('stroke', function (d, j) {
                     return color(i)
                 })
@@ -509,10 +823,8 @@ function switchToScene3() {
                 .on('mouseout', function () {
                     tooltip.html('')
                 })
-
-
             vis.selectAll('myCircles')
-                .data(projDataset)
+                .data(projSDSDataset)
                 .enter()
                 .append('circle')
                 .attr("fill", "lightgrey")
@@ -540,48 +852,9 @@ function switchToScene3() {
                 })
 
         })
-        //create plot of projected data
-        subProjGrouped.forEach(function (d, i) {
-            var pathData2 = lineGen(d.values);
-            vis.append('path')
-                .attr('d', pathData2)
-                .attr('class', 'line')
-                .attr('id', 'line_' + d.key)
-                .attr('stroke-width', 2)
-                .attr('stroke-dasharray', 4)
-                .attr('stroke', function (d, j) {
-                    return color(i)
-                })
-                .attr("fill", "none")
 
-                //create hover tooltip
-                .on('mouseover', function () {
-                    // console.log(d)
-                    tooltip.transition().duration(200)
-                        .style('opacity', .9)
-                    tooltip.html(
-                        '<div style="font-family:sans-serif; font-size: 1rem; font-weight: bold">' + d.key + '</div>'
-                    )
-                        .style('left', (d3.event.pageX - 35) + 'px')
-                        .style('top', (d3.event.pageY - 30) + 'px')
-                })
-                .on('mouseout', function () {
-                    tooltip.html('')
-                })
-
-
-        })
 
 
     })
 
-
-
-
-
-
-
-
-
 }
-
